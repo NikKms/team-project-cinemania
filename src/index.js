82ee247893f652511741b4bb4adc7c798bfa03af
NikKms <mykola.kamyshnikov@gmail.com>
2023-06-03 14:14:27 +0300

    correct html, add fonts, add styles.css, add root, add adaptive container

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import * as hero from './partials/js/hero/hero';
+import * as fonts from './partials/js/fonts';
---
+++ src/partials/js/fonts.js
@@ -0,0 +1,7 @@
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+// export { default as Roboto400 } from '@fontsource/roboto/400.css';
+// export { default as Roboto500 } from '@fontsource/roboto/500.css';
+// export { default as Roboto700 } from '@fontsource/roboto/700.css';
