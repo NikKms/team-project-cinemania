67f0ba174ad981a235d0bf1028bd478a8bace897
vadimfront <vadimzila94@gmail.com>
2023-06-04 22:02:11 +0300

    add handler form submit

--- src/partials/js/catalog/example.js
+++ src/partials/js/catalog/example.js
@@ -1,31 +1,31 @@
 import axios from 'axios';
 import { getByQuery, getMovie } from '../api';
+import { refs } from '../refs';
 import { createCards } from './createCards';
 
 const API_KEY = '88b8a7c5d221d3120fb29d734050dc7d';
+const searchForm = document.querySelector('.search-form');
 
-const createDataCards = async () => {
-  //////////////////////////////////////////
-  const str = 'ghosted';
-  const res = await getByQuery(str, 1);
-  const { results } = res;
-  //////////////////////////////////////////
+const submitHandler = e => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const { searchMovies } = Object.fromEntries(formData.entries());
+  getMoviesByQuery(searchMovies);
+};
 
+const getMoviesByQuery = async searchTerm => {
+  const { results } = await getByQuery(searchTerm, 1);
+  createDataCards(results).then(res => console.log(res));
+};
+
+const createDataCards = async moviesData => {
+  console.log(moviesData);
   const genre = await fetchGenre();
-  const newCardsDate = results.map(
-    ({
-      id,
-      title,
-      //original_language,
-      poster_path,
-      vote_average,
-      genre_ids,
-      release_date,
-    }) => {
+  const newCardsDate = moviesData.map(
+    ({ id, title, poster_path, vote_average, genre_ids, release_date }) => {
       return {
         id: id,
         title: title,
-        //language: original_language,
         poster: poster_path,
         rating: vote_average,
         genres: createGenreStr(genre, genre_ids),
@@ -60,6 +60,6 @@ const createMovieDate = dateString => {
   return year;
 };
 
-// createDataCards().then(res => console.log(res));
+searchForm.addEventListener('submit', submitHandler);
 
 export { createDataCards };
