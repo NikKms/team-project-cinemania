a605ba8da410a37c29999bdac9958301f226c002
Elvira Lemak <elvralema@rambler.ru>
2023-06-04 19:58:48 +0200

    add functions for home page, styles and markup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import * as hero from './partials/js/hero/hero';
+import * as hero from './partials/js/home/example-home';
 // import { Roboto400, Roboto500, Roboto700 } from './partials/js/fonts';
 // import '@fontsource/roboto/400.css';
 // import '@fontsource/roboto/500.css';
