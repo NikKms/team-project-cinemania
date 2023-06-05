0bba3949897578463012b6a8a9b6cf23fd855998
Annloveodessa <anjel1112@gmail.com>
2023-06-05 20:16:42 +0200

    herostyles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import * as hero from './partials/js/hero/hero';
+// import * as hero from './partials/js/hero/hero';
 import * as home from './partials/js/home/example-home';
 import * as header from './partials/js/header/header';
 import * as upBtn from './partials/js/up-btn'
