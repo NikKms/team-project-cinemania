2b8d4ab915eb3fb4e3ca7f7845ef20bfd0dbe450
vadimfront <vadimzila94@gmail.com>
2023-06-08 01:12:41 +0300

    style and fix some bugs

--- src/partials/js/catalog/createCards.js
+++ src/partials/js/catalog/createCards.js
@@ -3,27 +3,26 @@ import { refs } from '../refs';
 export const createCards = dataCard => {
   const markup = dataCard
     .map(card => {
-      return `<div data-id=${card.id} class="catalog-card">
+      return `<li data-id=${card.id} class="catalog-card">
       <img src='https://image.tmdb.org/t/p/original${card.poster}' alt="${
         card.title
       }" loading="lazy" />
       <div class="catalog-info">
-        <div>
-          <p class="catalog-info-name">
-            <b>${card.title}</b>
-          </p>
+      <p class="catalog-info-name">${card.title}</p>
+        <div class="catalog-info-container">
           <p class="catalog-info-details">
             <span>${card.genres}</span>
-            <span class="catalog-info-border" >${card.date}</span>
+            <span>${card.date}</span>
           </p>
-        </div>
-        <div
+          <div
           class="Stars"
           style="--rating: ${card.rating / 2}"
           aria-label="Rating of this product is 2.3 out of 5."
         ></div>
+        </div>
       </div>
-    </div>`;
+      <div class="overlay"></div>
+    </li>`;
     })
     .join('');
   addHTML(markup);
