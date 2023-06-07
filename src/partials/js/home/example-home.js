617bdd02f6d91accaba5c977b604b052338e917f
TiranoUser <v130561@gmail.com>
2023-06-07 23:25:07 +0200

    edit home.css all done

--- src/partials/js/home/example-home.js
+++ src/partials/js/home/example-home.js
@@ -53,23 +53,47 @@ async function renderWeeklyThreeTrends(firstThreeFilms) {
           />
          <div class="overlay"></div>       
         </div>
-        <div class="weekly-card-description">
-          <div>
-            <title class="weekly-card-description-title">${
-              title || name
-            }</title>
-            <p class="weekly-card-description-other">${listGenres} | ${
+  <div class="weekly-card-description">
+    <title class="weekly-card-description-title">${title || name}</title>
+    <div class='weekly-card-description-wrap'>
+       <p class="weekly-card-description-other">${listGenres} | ${
           release_date
             ? release_date.substring(0, 4)
             : first_air_date.substring(0, 4)
         }</p>
-          </div>
-         <div class="Stars" style="--rating: ${parseFloat(
-           (vote_average / 2).toFixed(1)
-         )};" aria-label="Rating of this product is 2.3 out of 5."></div> 
-        </div>
-        
+
+     <div class="Stars" style="--rating: ${parseFloat(
+       (vote_average / 2).toFixed(1)
+     )};" aria-label="Rating of this product is 2.3 out of 5."></div>
+      </div>
+  </div>
       </li>`;
+
+        //           return `<li class="weekly-card is-id" data-id=${id}>
+        //   <div class="weekly-container-image">
+        //     <img
+        //       class="weekly-card-image"
+        //      src="https://image.tmdb.org/t/p/original/${poster_path}"
+        //       alt=""
+        //     />
+        //    <div class="overlay"></div>
+        //   </div>
+        //   <div class="weekly-card-description">
+        //     <div>
+        //       <title class="weekly-card-description-title">${
+        //         title || name
+        //       }</title>
+        //       <p class="weekly-card-description-other">${listGenres} | ${
+        //             release_date
+        //               ? release_date.substring(0, 4)
+        //               : first_air_date.substring(0, 4)
+        //           }</p>
+        //     </div>
+        //    <div class="Stars" style="--rating: ${parseFloat(
+        //      (vote_average / 2).toFixed(1)
+        //    )};" aria-label="Rating of this product is 2.3 out of 5."></div>
+        //   </div>
+        // </li>`;
       }
     )
   );
