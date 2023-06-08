c3645bf5fd9bb788566529aadc2fdc9f858d7892
NikKms <mykola.kamyshnikov@gmail.com>
2023-06-08 18:09:09 +0300

    fix header

--- src/partials/js/header/header.js
+++ src/partials/js/header/header.js
@@ -1,7 +1,12 @@
+import * as modalMobile from '../header/header-modal';
+
 const navRefs = {
   navHome: document.querySelector('#nav_home'),
   navLib: document.querySelector('#nav_lib'),
   navCat: document.querySelector('#nav_cat'),
+  navHomeMob: document.querySelector('#nav_home_mob'),
+  navLibMob: document.querySelector('#nav_lib_mob'),
+  navCatMob: document.querySelector('#nav_cat_mob'),
 };
 document.addEventListener('DOMContentLoaded', function () {
   const checkbox = document.querySelector("input[type='checkbox']");
@@ -19,6 +24,7 @@ document.addEventListener('DOMContentLoaded', function () {
     localStorage.setItem('theme', theme);
     applyThemeStyles();
   });
+  pageNavSelector();
 });
 
 function applyThemeStyles() {
@@ -32,23 +38,35 @@ function applyThemeStyles() {
   });
 }
 
-pageNavSelector();
-
 function pageNavSelector() {
-  const body = document.getElementsByTagName('body')[0];
   if (window.location.href.includes('/index.html')) {
     navRefs.navHome.classList.add('current-page');
+    navRefs.navHomeMob.classList.add('current-page');
+
     navRefs.navLib.classList.remove('current-page');
+    navRefs.navLibMob.classList.remove('current-page');
+
     navRefs.navCat.classList.remove('current-page');
+    navRefs.navCatMob.classList.remove('current-page');
   }
   if (window.location.href.includes('/catalog.html')) {
     navRefs.navHome.classList.remove('current-page');
+    navRefs.navHomeMob.classList.remove;
+
     navRefs.navLib.classList.remove('current-page');
+    navRefs.navLibMob.classList.remove('current-page');
+
     navRefs.navCat.classList.add('current-page');
+    navRefs.navCatMob.classList.add('current-page');
   }
   if (window.location.href.includes('/my-lib-page.html')) {
     navRefs.navHome.classList.remove('current-page');
+    navRefs.navHomeMob.classList.remove('current-page');
+
     navRefs.navLib.classList.add('current-page');
+    navRefs.navLibMob.classList.add('current-page');
+
     navRefs.navCat.classList.remove('current-page');
+    navRefs.navCatMob.classList.remove('current-page');
   }
 }
